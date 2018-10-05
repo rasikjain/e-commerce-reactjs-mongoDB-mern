c28af0d60ffff5865e56143e31b7905092663f7f
Rasik Jain <jainrasik@gmail.com>
2018-10-05 16:54:32 +0530

    adding controllers and test cases

--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
+//Configure the env file
 require("dotenv").config();
+//Middlewares
+//Configure body-parser so you can retrieve values from the req.body, if not the req.body will be undefined.
 const bodyParser = require("body-parser");
+//Require the session for saving user data and giving a user a unique experience.
 const session = require("express-session");
+//Use cors for enable cross origin sharign
 const cors = require("cors");
-
+//
 //Controllers
 //These are how we define our endpoints
 //Note can name all the variable anything you want  I am just operating on convention or best practice.
 //Set your admin functionality
 const adminController = require("./controllers/admin_controller");
 //Set your cloudinary functionality
-//const cloudinaryController = require("./controllers/cloudinary_controller");
+const cloudinaryController = require("./controllers/cloudinary_controller");
 //Set your user functionality.
-//const userController = require("./controllers/user_controller");
+const userController = require("./controllers/user_controller");
 //Set your products functionality.
 const productsController = require("./controllers/products_controller");
-
+//
 //Import your mongoose module to connect to your mongodb database instance using it's connection string.
 const mongoose = require("mongoose");
 //Import your express server
@@ -24,7 +29,6 @@ const app = express();
 //Define the Port your will be running your server on.
 //NOTE: Make sure the POrt is the same as the proxy.
 const PORT = 5050;
-console.log(process.env.CONNECTION_STRING);
 //Connect the mongoose to the database using it's connect method.
 mongoose.connect(
   process.env.CONNECTION_STRING,
@@ -57,30 +61,31 @@ app.use(
   })
 );
 //Allow cross origin requests.
-app.use(cors());
+//  app.use(cors());
 
 //User endpoints
 //Use when retrieving user data from request session. We middleware we defined earlier.
 setTimeout(() => {
   //Now setup cloudinary this endpoint will get the credentials from cloudinary_controller which will be signed.
-  //app.get("/api/upload", cloudinaryController.upload);
+  app.get("/api/upload", cloudinaryController.upload);
+
   //Read the user's session.
-  //app.get("/api/user-data", userController.readUserData);
+  app.get("/api/user-data", userController.readUserData);
 
   //Add a item to cart.
-  //app.post("/api/user-data/cart", userController.addToCart);
+  app.post("/api/user-data/cart", userController.addToCart);
 
   //Remove a item from the cart.
   // Use request parameter to remove item from cart since you are looking a specific item in cart.
-  //app.delete("/api/user-data/cart/:id", userController.removeFromCart);
+  app.delete("/api/user-data/cart/:id", userController.removeFromCart);
 
   //When user login
-  //app.get("/auth/callback", userController.login);
+  app.get("/auth/callback", userController.login);
   //NO NEED FOR A REGISTER SINCE YOUR ARE USING AUTH0.
   //Just need a login, since you are logging from your social media provider no need to register, only looks if a user already has a account.
 
   //When the user logouts
-  //app.post("/api/logout", userController.logout);
+  app.post("/api/logout", userController.logout);
 
   //Products Endpoints
   //Getting all the products
